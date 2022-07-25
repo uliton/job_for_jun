dba90ad87891072f9178f2b82d11fb359fcbb652
Alex <al.haruca@gmail.com>
2022-07-25 11:04:00 +0200

    add search bar and clear button

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { Header } from './components/Header';
+import React, { useState } from 'react';
 import companies from './companiesBase.json';
+import { Header } from './components/Header';
+import { SearchBar } from './components/SearchBar';
 import { Company } from './components/Company';
 
 import styles from './App.module.scss';
 
-export const App = () => (
-  <div className={styles.body}>
-    <Header />
-    <div className={styles.flex}>
-      {companies.map(company => (
-        <Company
-          key={company.id}
-          id={company.id}
-          name={company.companyName}
-          logo={company.logo}
-          link={company.website}
-          linkedin={company.linkedin}
-          dou={company.dou}
-        />
-      ))}
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const filteredCompanies = companies.filter(
+    conpany => conpany.companyName.toLowerCase().includes(query.toLowerCase()),
+  );
+
+  return (
+    <div className={styles.body}>
+      <Header />
+      <SearchBar query={query} setQuery={setQuery} />
+      <div className={styles.flex}>
+        {filteredCompanies.map(company => (
+          <Company
+            key={company.id}
+            id={company.id}
+            name={company.companyName}
+            logo={company.logo}
+            link={company.website}
+            linkedin={company.linkedin}
+            dou={company.dou}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
