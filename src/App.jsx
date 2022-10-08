7767c5d58a12f03c54514773853c0cc239d0b942
Oleksandr <al.haruca@gmail.com>
2022-10-08 23:22:32 +0200

    add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { Main } from './components/Main/Main';
+import { NotFound } from './components/NotFound';
 import { OtherProjects } from './components/OtherProjects';
 
 export const App = () => (
   <Routes>
     <Route path="/" element={<Main />} />
     <Route path="/job_for_jun/" element={<Main />} />
-    <Route path="/op" element={<OtherProjects />} />
+    <Route path="/job_for_jun/op" element={<OtherProjects />} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 );
